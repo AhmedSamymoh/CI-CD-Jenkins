2779b45c3cfc8a87711645501b6b74b81f19c4cf
Rana Mohamed <144630250+RanaMohamed227@users.noreply.github.com>
2024-07-18 22:11:52 +0300

    Add files via upload

---
+++ Knowledge/UnitTesting/Gtest/build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/tests.cpp.obj"
+  "CMakeFiles/runTests.dir/tests.cpp.obj.d"
+  "librunTests.dll.a"
+  "runTests.exe"
+  "runTests.exe.manifest"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
