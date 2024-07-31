d79c29e06236a849d9e49755d3e65feacb3c98eb
AhmedSamymoh <ahmeds.elnozahy@gmail.com>
2024-07-31 11:06:17 +0300

    Updating batch script

--- Knowledge/Jira_Integration/import_csv/SRC/Jira.bat
+++ Knowledge/Jira_Integration/import_csv/SRC/Jira.bat
@@ -2,33 +2,38 @@
 setlocal
 
 REM Set your Jira credentials and endpoint
-set "JIRA_USER=ranamohamed2722003@gmail.com"
-set "JIRA_API_TOKEN=ATATT3xFfGF0_vSjuzRXb76JcaFwbD8ReV00bNWBZLxc9sCh8w9x_g8SZSo2q2FB9QpgpdH9tArg38310C7FMN2vM_89hJGH9gi2vfRsA67RCyk9xR9JAyH0OOFG0UF7j_PL-tQ_cE9588NHZ88LY6HoaGxQDyCxgR-I00_xbCaEAWOhH7LRNSw=F7021664"
+set "JIRA_USER=ahmeds.elnozahy@gmail.com"
+set "JIRA_API_TOKEN=ATATT3xFfGF0hAyV6gXAlbYVNZfU5drSW-nUtEeitDa1SEg4frpd50-LOr48uJIUGCgtyRlgah_T740q_kbj45x6HLRuBR6FPnleO_tZQ3f1jNITL6myMBJUyQIjDHa_TLFvHkfdtR2Ui88NV_GrENwq-BWwspdzk8yLvRCbLSc2OjQwluMR_9U=319AB789"
 set "JIRA_URL=https://ahmedselnozahy.atlassian.net/rest/api/2/search?jql=project=CCB"
 
 REM Output file
-set "OUTPUT_FILE=jira_all.csv"
+set "OUTPUT_FILE=jira_all_issues.csv"
 
 REM Make API request and save the response to a file
 curl -u %JIRA_USER%:%JIRA_API_TOKEN% -X GET -H "Content-Type: application/json" %JIRA_URL% -o response.json
 
 REM Check if the response contains an error
-"C:\Program Files\jq\jq.exe" -e .errors response.json >nul 2>&1
+jq -e .errors response.json >nul 2>&1
 if %ERRORLEVEL% equ 0 (
     echo Error in response
     type response.json
     exit /b 1
 )
-
 REM Process the JSON response to extract the data and save it as CSV
-
-"C:\Program Files\jq\jq.exe" -r ".issues[] | [.key, .fields.summary, .fields.assignee.displayName, .fields.status.name,  (.fields.customfield_10020[0] | .name ), .fields.created, .fields.updated] | @csv" response.json > %OUTPUT_FILE%
-
+(
+echo "KEY","SPRINT","SUMMARY","ASSIGNEE", "STATUS","START DATE","UPDATED"
+jq -r ".issues[] | [.key, .fields.customfield_10020[0].name  , .fields.summary, .fields.assignee.displayName, .fields.status.name, .fields.created, .fields.updated] | @csv" response.json
+) > %OUTPUT_FILE%
 
 REM Clean up
 del response.json
 
 echo CSV file has been saved to %OUTPUT_FILE%
+
+echo HighestSprint.py Running!
+python HighestSprint.py
+
+
 
 endlocal
 pause
