fc36ad5e6eae6c2100854a653522eba19c2e8f18
AhmedSamymoh <ahmeds.elnozahy@gmail.com>
2024-08-18 20:33:39 +0300

    Updating Misra.sh: - V2.0

--- Knowledge/UnitTesting/Static Code Analysis/Misra.sh
+++ Knowledge/UnitTesting/Static Code Analysis/Misra.sh
@@ -1,40 +1,70 @@
 #!/bin/bash
-#
-# @Misra.sh
+#===================================================================================================
+# @Misra.sh:
 # This script automates the MISRA C 2012 compliance checking using the open-source cppcheck tool.
 # It applies the MISRA C 2012 rules specified in the 'misra.json' configuration file.
 # The script performs the following steps:
-# 1. Validates that exactly one .c source file is provided as an argument.
-# 2. Runs cppcheck on the provided source file with MISRA C 2012 rules applied, 
+# 1. Validates that exactly one .c source file is provided .
+# 2. Runs cppcheck on the provided source file with MISRA C 2012 rules applied,
 #    and saves the output to 'MisraCkOut.txt'.
 # 3. Displays the cppcheck output.
-# 4. Executes 'CheckMisraC.py' to parse 'MisraCkOut.txt' and determine if any mandatory 
-#    rule violations are present. The Jenkins stage will fail if violations are found.
+# 4. Executes 'CheckMisraC.py' to parse 'MisraCkOut.txt' and determine if any mandatory       
+#    rule violations are present. The Jenkins stage will fail if violations are found.        
 # @Aurthor : Ahmed Samy
+#===================================================================================================
 # Usage:
-# ./Misra.sh <source_file.c>
+# ./Misra.sh <misra_source_files.txt>
+#
+# <misra_source_files.txt> :
+#   /pathinrepo/src/source_file1.c
+#   /pathinrepo/src/source_file2.c
+#   /pathinrepo/src/source_file3.c
+#
+#===================================================================================================
 
 
-# Check if the user has provided a .c file as an argument
+misra_source_files="$1"
+# Function to check if the <misra_source_files.txt> file exists and is not empty
+source_list_check() {
+    if [ ! -f "$misra_source_files" ] || [ ! -s "$misra_source_files" ]; then
+        echo "Error: File '$misra_source_files' does not exist or is empty."
+        exit 1
+    fi
+}
+
+
+# Function to parse the source files listed in <misra_source_files.txt>
+parse_and_run_cppcheck() {
+
+    source_list_check 
+
+    # Process each file listed in the source file list
+    while IFS= read -r SOURCE_FILE; do
+
+        # Check if the file exists and is not empty
+        if [ ! -f "$SOURCE_FILE" ] || [ ! -s "$SOURCE_FILE" ]; then
+            echo "Error: File '$SOURCE_FILE' does not exist or is empty."
+            continue
+        fi
+        echo "Misra C Checking ===> : $SOURCE_FILE ====================================="
+        # Run cppcheck with the provided source file
+        cppcheck --addon="/var/jenkins_home/MisraC/misra.json" --suppress=missingIncludeSystem "$SOURCE_FILE" > MisraCkOut.txt 2>&1
+
+        # Display the cppcheck output
+        cat MisraCkOut.txt
+
+        # Run the Python script to parse the cppcheck output
+        python3 /var/jenkins_home/MisraC/CheckMisraC.py
+        echo "=============> process exit with $? ======================================="
+
+    done < "$misra_source_files"
+}
+
+# Main script logic
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <source_file.c>"
+    echo "Usage: $0 <misra_source_files.txt>"
     exit 1
 fi
 
-# Get the source file from the argument
-SOURCE_FILE="$1"
-
-# Check if the file exists and is not empty
-if [ ! -f "$SOURCE_FILE" ] || [ ! -s "$SOURCE_FILE" ]; then
-    echo "Error: File '$SOURCE_FILE' does not exist or is empty."
-    exit 1
-fi
-
-# Run cppcheck with the provided source file
-cppcheck --addon="/var/jenkins_home/MisraC/misra.json" --suppress=missingIncludeSystem "$SOURCE_FILE" > MisraCkOut.txt 2>&1 
-
-# Display the cppcheck output
-cat MisraCkOut.txt
-
-# Run the Python script
-python3 /var/jenkins_home/MisraC/CheckMisraC.py
+# Call the function with the provided file
+parse_and_run_cppcheck "$1"
